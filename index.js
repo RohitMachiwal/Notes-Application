c344b9e26b9fc8e6776e2c6e31834bdccf12061d
Rohit Machiwal <87191348+RohitMachiwal@users.noreply.github.com>
2022-02-18 12:07:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+/*
+console.log('Hello Rohit');
+console.log(4+4);
+console.warn('Warning');
+console.table({Rohit: 'this',Marks:34})
+console.timeEnd('Your coder took');
+//console.assert(2>3, 'This is not possible');
+ console.error('Errorr');
+
+ */
+
+
+ var name='Rohit';
+
+ var marks=34;
+
+ console.log(name,marks);
+
+ const a=14;
+ // a=6;
+
+ console.log(a);
+
+ var city='delhi'
+ {
+
+    let city='Faridabad';
+    
+    console.log(city);
+ }
+ console.log(city);
+
+ const arr=[12,15,17,19];
+ console.log(arr);
+
+ arr.push(55);
+ console.log(arr);
+
+
+ // Datatypes
+ const name2= 'ram';
+const name1 = "hari";
+const result = `The names are ${name2} and ${name1}`;
+
+console.log(result);
+
+// BigInt value
+const value1 = 900719925124740998n;
+
+// Adding two big integers
+const result1 = value1 + 1n;
+console.log(result1); // "900719925124740999n"
+
+const value2 = 900719925124740998n;
+
+// Error! BitInt and number cannot be added
+//const result2 = value2 + 1; 
+//console.log(result2); 
+
